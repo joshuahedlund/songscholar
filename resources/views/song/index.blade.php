6cc451524f4874836ced7635d2afaa6633fac36a
joshuahedlund <joshedlund@gmail.com>
2016-08-28 20:24:20 -0500

    Improve passage UI

--- resources/views/song/index.blade.php
+++ resources/views/song/index.blade.php
@@ -40,16 +40,7 @@
                                 
                         <td>
                             <div id="editPassage-{{$songRef->id}}"> 
-                                <p>
-                                    @if($passage) {{$passage->book}} {{$passage->chapter}}:{{$passage->verse}} @endif
-                                    @if(!Auth::guest())
-                                    [<a href="javascript:void(0);" onclick="ajaxEditPassageReference({{$songRef->id}});">correct this reference</a>]
-                                    [<a href="javascript:void(0);" onclick="ajaxEditPassageVersion({{$songRef->id}});">correct this version</a>]
-                                    @endif
-                                </p>
-                                <p>
-                                    <?php echo nl2br($songRef->passageVersion->text); ?> ({{$songRef->passageVersion->version }})
-                                </p>
+                                @include('song.indexPassage')
                             </div>
                         </td>
                     </tr>
--- resources/views/song/indexPassage.blade.php
+++ resources/views/song/indexPassage.blade.php
@@ -1,11 +1,13 @@
 <?php $passage = $songRef->passageVersion->passage; ?>
                                 <p>
-                                    @if($passage) {{$passage->book}} {{$passage->chapter}}:{{$passage->verse}} @endif
+                                    <?php echo nl2br($songRef->passageVersion->text); ?> ({{$songRef->passageVersion->version }})
+                                </p>
+                                <p>
+                                    @if($passage) {{ HTML::linkAction('BookController@index',$passage->book,[str_replace(' ','-',$passage->book)]) }}
+                                        {{$passage->chapter}}:{{$passage->verse}}
+                                    @endif
                                     @if(!Auth::guest())
                                     [<a href="javascript:void(0);" onclick="ajaxEditPassageReference({{$songRef->id}});">correct this reference</a>]
                                     [<a href="javascript:void(0);" onclick="ajaxEditPassageVersion({{$songRef->id}});">correct this version</a>]
                                     @endif
-                                </p>
-                                <p>
-                                    <?php echo nl2br($songRef->passageVersion->text); ?> ({{$songRef->passageVersion->version }})
                                 </p>
