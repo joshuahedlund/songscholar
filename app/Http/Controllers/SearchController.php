57f3c7185fd046a9dccc82c2a1d4084819be7403
joshuahedlund <joshedlund@gmail.com>
2016-11-01 19:38:13 -0500

    Search support exact book and chapter

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,12 +16,42 @@ class SearchController extends Controller
      */
     public function index(Request $request)
     {
-        //Get artists with count of references per artist
-        $data['searchTerm'] = $request->search;
-            
-        $data['artists'] = \App\Artist::whereRaw("MATCH (name) AGAINST (:search IN BOOLEAN MODE)",['search'=>$request->search])->orderBy('name')->get();
-        $data['songs'] = \App\Song::whereRaw("MATCH (name) AGAINST (:search IN BOOLEAN MODE)",['search'=>$request->search])->orderBy('name')->get();
+        $cntArtist=0;
+        $cntBook=0;
+        $cntSongs=0;
+        $cnRefs=0;
+                        
         
+        $data['searchTerm'] = $request->search;
+        
+        //Find matching book/chapter
+        $searchBook = $request->search;
+        $searchCh = 0;
+        //See if last word is chapter
+        $searchWords = explode(' ',$request->search);
+        $lastWord = count($searchWords);
+        if(count($searchWords)>1 && is_numeric($searchCh = array_pop($searchWords))){ //if last word is numeric, pop it off (assume as chapter) and...
+            $searchBook = implode(' ',$searchWords); //put the rest back together for the book
+        }
+        //Search for book, either original string or with chapter popped off
+        $book = \App\Book::where('name',$searchBook)->first();
+        if($book){
+            $bookUrl = '/book/'.str_replace(' ','-',$book->name);
+            if($searchCh){
+                $bookUrl .= '/'.$searchCh;
+            }
+            return redirect($bookUrl);
+        }
+            
+        //Find matching artists
+        $data['artists'] = \App\Artist::whereRaw("MATCH (name) AGAINST (:search IN BOOLEAN MODE)",['search'=>$request->search])->orderBy('name')->get();
+        $cntArtist = count($data['artists']);
+        
+        //Find matching songs
+        $data['songs'] = \App\Song::whereRaw("MATCH (name) AGAINST (:search IN BOOLEAN MODE)",['search'=>$request->search])->orderBy('name')->get();
+        $cntSongs = count($data['songs']);
+        
+        //Find matching lyrics or text
         $match = 'MATCH (songRefs.lyric) AGAINST(:lyric IN BOOLEAN MODE) OR MATCH (passageVersions.text) AGAINST(:text IN BOOLEAN MODE)';
         $data['songRefs'] = DB::table('songRefs')
             ->join('songs','songRefs.song_id','=','songs.id')
@@ -33,6 +63,7 @@ class SearchController extends Controller
             ->havingRaw('score > 0')
             ->orderBy('score','desc')
             ->get();
+        $cntRefs = count($data['songRefs']);
         
         return view('search.index', $data);
     }
